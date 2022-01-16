9331977689e49a59cded336247f8cd54ae989b94
Andrey "MOU" Larionov <anlarionov@gmail.com>
2022-01-16 16:30:05 +0300

    Created basic nix flake
    Also added missed mtprof dependency and removed
    not necessary anymore setup tools install script

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    {
+      # Nixpkgs overlay providing the application
+      overlay = nixpkgs.lib.composeManyExtensions [
+        poetry2nix.overlay
+        (final: prev: {
+          # The application
+          uengine = prev.poetry2nix.mkPoetryApplication {
+            projectDir = ./.;
+          };
+        })
+      ];
+    } // (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+      in
+      {
+        apps = {
+          uengine = pkgs.uengine;
+        };
+
+        defaultApp = pkgs.uengine;
+
+        packages = {
+          uengine = pkgs.uengine;
+        };
+
+        defaultPackage = pkgs.uengine;
+
+        devShell = pkgs.uengine.dependencyEnv;
+      }));
+}
